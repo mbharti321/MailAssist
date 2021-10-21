19f23517dbe33604e0f3b53374571db52bb2d70b
Dharamraj Bhatt <dharamrajbhatt97@gmail.com>
2021-10-22 02:36:16 +0530

    ui chnages

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'Mail.dart';
+import 'MailGenerator.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -37,11 +39,11 @@ class FirstScreen extends StatelessWidget {
             width: 220,
             padding: EdgeInsets.all(7),
             decoration: BoxDecoration(
-                color: Colors.white,
+                color: Colors.blue,
                 borderRadius: BorderRadius.all(Radius.circular(8))),
             child: TextButton.icon(
               style: TextButton.styleFrom(
-                textStyle: TextStyle(color: Colors.blue),
+                textStyle: TextStyle(color: Colors.black),
                 backgroundColor: Colors.white,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(24.0),
@@ -57,7 +59,8 @@ class FirstScreen extends StatelessWidget {
               },
 
               icon: Icon(
-                Icons.send_rounded,
+                //Icons.send_rounded,
+                Icons.attach_email_outlined,
               ),
               // icon: Image.asset('assets/images/icons/google.jpg'),
               //Icon(
@@ -67,19 +70,7 @@ class FirstScreen extends StatelessWidget {
               label: Text(
                 'Sign in with Google',
               ),
-              // onPressed: () {
-              //   Navigator.push(
-              //     context,
-              //     MaterialPageRoute(builder: (context) => MyApp1()),
-              //   );
-              // },
             ),
-            // child: TextButton(
-            //   onPressed: () => signInWithGoogle(),
-            //   child: Text(
-            //     "sign in with Google",
-            //     style: TextStyle(color: Colors.red),
-            //   ),
           ),
         ),
       ),
@@ -118,11 +109,67 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  @override
+  var drawerIcons = [
+    Icon(Icons.move_to_inbox),
+    Icon(Icons.inbox),
+    Icon(Icons.people),
+    Icon(Icons.local_offer),
+    Icon(Icons.star),
+    Icon(Icons.access_time),
+    Icon(Icons.label),
+    Icon(Icons.send),
+    Icon(Icons.send),
+    Icon(Icons.note),
+    Icon(Icons.mail),
+    Icon(Icons.error),
+    Icon(Icons.delete),
+    Icon(Icons.label),
+    Icon(Icons.label),
+    Icon(Icons.settings),
+    Icon(Icons.error),
+  ];
+
+  var drawerText = [
+    "All inboxes",
+    "Primary",
+    "Social",
+    "Promotions",
+    "Starred",
+    "Snoozed",
+    "Important",
+    "Sent",
+    "Outbox",
+    "Drafts",
+    "All mail",
+    "Spam",
+    "Bin",
+    // "[Imap]/Sent",
+    // "[Imap]/Trash",
+    "Settings",
+    "Log Out"
+  ];
+
+  var titleBarContent = "Primary";
+
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: _getMailAppBar(),
       drawer: _getMailAccountDrawerr(),
+      body: _mailListViewGenerator(),
+      floatingActionButton: _getMailFloatingActionButton(),
+    );
+  }
+
+  FloatingActionButton _getMailFloatingActionButton() {
+    return FloatingActionButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ComposeEmailPageState()),
+        );
+      },
+      child: Icon(Icons.edit),
+      backgroundColor: Colors.red,
     );
   }
 
@@ -151,7 +198,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Drawer _getMailAccountDrawerr() {
     Text email = new Text(
-      "dharamrajbhatt97@gmail.com.com",
+      "dharamrajbhatt97@gmail.com",
       style: TextStyle(fontWeight: FontWeight.w500, fontSize: 15.0),
     );
 
@@ -164,7 +211,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
       children: <Widget>[
         UserAccountsDrawerHeader(
-          decoration: BoxDecoration(color: Colors.orangeAccent),
+          decoration: BoxDecoration(color: Colors.orange),
           accountName: name,
           accountEmail: email,
           currentAccountPicture: Icon(
@@ -173,7 +220,203 @@ class _MyHomePageState extends State<MyHomePage> {
             color: Colors.white,
           ),
         ),
+        Expanded(
+          flex: 2,
+          child: ListView.builder(
+              padding: EdgeInsets.only(top: 0.0),
+              itemCount: drawerText.length,
+              itemBuilder: (context, position) {
+                return ListTile(
+                  leading: drawerIcons[position],
+                  title: Text(drawerText[position],
+                      style: TextStyle(fontSize: 15.0)),
+                  onTap: () {
+                    this.setState(() {
+                      titleBarContent = drawerText[position];
+                    });
+                    Navigator.pop(context);
+                  },
+                );
+              }),
+        )
       ],
     ));
+  }
+
+  Widget _mailListViewGenerator() {
+    return ListView.builder(
+        itemCount: MailGenerator.mailListLength,
+        itemBuilder: (context, position) {
+          MailContent mailContent = MailGenerator.getMailContent(position);
+          return Column(children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(
+                  left: 14.0, right: 14.0, top: 5.0, bottom: 5.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.account_circle,
+                    size: 55.0,
+                    color: Colors.red,
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 10.0),
+                      child: Column(
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text(
+                                mailContent.sender,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.black87,
+                                    fontSize: 17.0),
+                              ),
+                              Text(
+                                mailContent.time,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.black54,
+                                    fontSize: 13.5),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    mailContent.subject,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w400,
+                                        color: Colors.black54,
+                                        fontSize: 15.5),
+                                  ),
+                                  Text(
+                                    mailContent.message,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w400,
+                                        color: Colors.black54,
+                                        fontSize: 15.5),
+                                  )
+                                ],
+                              ),
+                              Icon(Icons.star_border, size: 25.0),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Divider(),
+          ]);
+        });
+  }
+}
+
+class ComposeEmailPageState extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          leading: GestureDetector(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 4.0),
+              child: Icon(Icons.arrow_back, color: Colors.white, size: 25.0),
+            ),
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          title: new Text(
+            "Compose",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 21.0,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 13.0),
+              child: Icon(
+                Icons.attachment,
+                size: 25.0,
+                color: Colors.white,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 13.0),
+              child: Icon(
+                Icons.send,
+                size: 23.0,
+                color: Colors.white,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 13.0),
+              child: Icon(
+                Icons.more_vert,
+                size: 25.0,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+        body: Column(
+          children: <Widget>[
+            ListTile(
+              title: TextField(
+                  decoration: InputDecoration(
+                labelText: "From",
+                border: InputBorder.none,
+                labelStyle: TextStyle(color: Colors.black54, fontSize: 18.0),
+              )),
+              trailing: Icon(Icons.keyboard_arrow_down),
+            ),
+            Divider(),
+            ListTile(
+              title: TextField(
+                  decoration: InputDecoration(
+                      labelText: "To",
+                      border: InputBorder.none,
+                      labelStyle:
+                          TextStyle(color: Colors.black54, fontSize: 18.0))),
+              trailing: Icon(Icons.keyboard_arrow_down),
+            ),
+            Divider(),
+            ListTile(
+              title: TextField(
+                  decoration: InputDecoration(
+                      hintText: "Subject",
+                      border: InputBorder.none,
+                      hintStyle:
+                          TextStyle(color: Colors.black54, fontSize: 18.0))),
+            ),
+            Divider(),
+            Expanded(
+              flex: 4,
+              child: ListTile(
+                title: TextField(
+                    decoration: InputDecoration(
+                  hintText: "Compose Email",
+                  border: InputBorder.none,
+                  hintStyle: TextStyle(color: Colors.black54, fontSize: 18.0),
+                )),
+              ),
+            ),
+            Divider(),
+          ],
+        ));
   }
 }
